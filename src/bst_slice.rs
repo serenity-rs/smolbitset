643f56946a3a45973a31a3787e34c9f49944b62e
DPlayer234 <dplayer.235@gmail.com>
2025-11-19 13:22:36 +0100

    ensure Eq and Ord use logical instead of physical value

---
+++ src/bst_slice.rs
@@ -0,0 +1,31 @@
+#[cfg(target_pointer_width = "64")]
+use crate::BST_BITS;
+use crate::{BitSliceType, INLINE_SLICE_PARTS, SmolBitSet};
+
+pub enum BstSlice<'a> {
+    Inline([BitSliceType; INLINE_SLICE_PARTS]),
+    Heap(&'a [BitSliceType]),
+}
+
+impl<'a> BstSlice<'a> {
+    pub fn new(sbs: &'a SmolBitSet) -> Self {
+        if sbs.is_inline() {
+            let data = unsafe { sbs.get_inline_data_unchecked() };
+            #[cfg(target_pointer_width = "32")]
+            let data = [data as BitSliceType];
+            #[cfg(target_pointer_width = "64")]
+            let data = [(data as BitSliceType), ((data >> BST_BITS) as BitSliceType)];
+            return Self::Inline(data);
+        } else {
+            let slice = unsafe { sbs.as_slice_unchecked() };
+            return Self::Heap(slice);
+        }
+    }
+
+    pub fn slice(&self) -> &[BitSliceType] {
+        match self {
+            Self::Inline(items) => items,
+            Self::Heap(items) => *items,
+        }
+    }
+}
